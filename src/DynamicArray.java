7c822a0e89de03459ec79b4e218b711e6a533c77
Amanuel Aman Yemane Tesema <aayt@kth.se>
2025-09-09 11:35:44 +0200

    Bra början, vi bheöver testa

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -4,43 +4,59 @@ import java.util.stream.Collectors;
 
 public class DynamicArray {
     Node root;
+    int height;
 
-    public DynamicArray() {
-        this.root = null;
-    }
 
-    private DynamicArray(Node root){
+    public DynamicArray(Node root){
         this.root = root;
+        this.height = 0;
     }
     
     public DynamicArray newarray() {
-        DynamicArray newArray = new DynamicArray();
+        DynamicArray newArray = new DynamicArray(null);
         return newArray;
     }
 
-    public DynamicArray set(int i, int value) {
-        Node newRoot = updateNode(root, i, value, 31);
+    // public static DynamicArray newarray() {
+    //     return new DynamicArray(null);
+    // }
+    
+
+    public DynamicArray set(DynamicArray a, int i, int value) {
+        Node newRoot = updateNode(a.root, i, value, a.height, a.height);
         return new DynamicArray(newRoot);
     }
 
-    private static Node updateNode(Node node, int i, int value, int bit) {
-        boolean bitIsOne = ((i >>> bit) & 1) == 1; // Shifts MSB to the LSB and masks that bit
+    private static Node updateNode(Node root, int i, int value, int height, int bit) {
         if (bit < 0) {
             return new Node(value, null, null); // New leaf is created
         }
-        if (node == null){
-            return new Node(null, null, null); // Creates path to index if there isn't an existing one
-        } 
-        if (bitIsOne) {
-            return new Node(node.value, node.left, updateNode(node.right, i, value, bit- 1)); // Keeps the left side, and updates the right side
+        
+        boolean updateHeight = (Math.pow(2, height)<i);
+        boolean bitIsOne = ((i >>> bit) & 1) == 1; // Shifts MSB to the LSB and masks that bit
+
+        if (root == null){
+          return new Node(null, null, null); // Creates path to index if there isn't an existing one
+        } // ATT HA KOLL PÅ: Ett nytt löv kan hamna på en lägre nivå än de löv som finns då de inte updateras till en lägre nivå
+
+
+        if (updateHeight) {
+            Node newRoot = new Node(null, root, null);
+            return new Node(newRoot.value, root, updateNode(newRoot.right, i, value, height+1, bit));
+        } else if (bitIsOne) {
+            return new Node(root.value, root.left, updateNode(root.right, i, value, height, bit-1)); // Keeps the left side, and updates the right side
         } else {
-            return new Node(node.value, updateNode(node.left, i, value, bit - 1), node.right);
+            return new Node(root.value, updateNode(root.left, i, value, height, bit-1), root.right);
         }
     }
+    
+    
+    
 
     public int get(DynamicArray a, int i) {
+
         Node node=a.root;
-        for (int bit = 31; bit >= 0; bit--) {
+        for (int bit =a.height; bit >= 0; bit--) {
             if (((i >>> bit) & 1) == 1){
                 node=node.right;
             } else {
@@ -50,9 +66,14 @@ public class DynamicArray {
     }
 
     public static void main(String[] args) {
-        Stack treeStack;
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = reader.lines().collect(Collectors.joining("\n"));
+        DynamicArray tree = new DynamicArray(null);
+        tree.set(tree, 3, 5);
+        tree.get(tree, 3);
+
+        // Stack treeStack;
+
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // String input = reader.lines().collect(Collectors.joining("\n"));
     }
 }
