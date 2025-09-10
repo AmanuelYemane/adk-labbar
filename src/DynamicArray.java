728fd2a4c7ebfff0902ae1ec0195953978f4af39
Mattias Nyberg <mattiasnyberg@outlook.com>
2025-09-10 11:17:15 +0200

    ändrar height hantering

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -4,16 +4,13 @@ import java.util.stream.Collectors;
 
 public class DynamicArray {
     Node root;
-    int height;
 
-
-    public DynamicArray(Node root){
-        this.root = root;
-        this.height = 0;
+    public DynamicArray(Node root) {
+        this.root = new Node(null, null, null, 0);
     }
     
     public DynamicArray newarray() {
-        DynamicArray newArray = new DynamicArray(null);
+        DynamicArray newArray = new DynamicArray(root);
         return newArray;
     }
 
@@ -23,53 +20,59 @@ public class DynamicArray {
     
 
     public DynamicArray set(DynamicArray a, int i, int value) {
-        Node newRoot = updateNode(a.root, i, value, a.height, a.height);
+        Node newRoot = updateNode(a.root, i, value, a.root.height, a.root.height);
         return new DynamicArray(newRoot);
     }
 
     private static Node updateNode(Node root, int i, int value, int height, int bit) {
         if (bit < 0) {
-            return new Node(value, null, null); // New leaf is created
+            return new Node(value, null, null, height); // New leaf is created
         }
         
-        boolean updateHeight = (Math.pow(2, height)<i);
+        boolean updateHeight = (Math.pow(2, height) < i);
         boolean bitIsOne = ((i >>> bit) & 1) == 1; // Shifts MSB to the LSB and masks that bit
 
+        if (root == null && bit == 0) {
+            return new Node(value, null, null, height); // skapar löv med rätt värde
+        }
+
         if (root == null){
-          return new Node(null, null, null); // Creates path to index if there isn't an existing one
+          return new Node(null, null, null, height); // Creates path to index if there isn't an existing one
         } // ATT HA KOLL PÅ: Ett nytt löv kan hamna på en lägre nivå än de löv som finns då de inte updateras till en lägre nivå
 
-
         if (updateHeight) {
-            Node newRoot = new Node(null, root, null);
-            return new Node(newRoot.value, root, updateNode(newRoot.right, i, value, height+1, bit));
+            Node newRoot = new Node(null, root, null, height + 1);
+            return newRoot;
+            Node(newRoot.value, root, updateNode(newRoot.right, i, value, height + 1, bit), height + 1);
         } else if (bitIsOne) {
-            return new Node(root.value, root.left, updateNode(root.right, i, value, height, bit-1)); // Keeps the left side, and updates the right side
+            return new Node(root.value, root.left, updateNode(root.right, i, value, height, bit - 1)); // Keeps the left side, and updates the right side
         } else {
-            return new Node(root.value, updateNode(root.left, i, value, height, bit-1), root.right);
+            return new Node(root.value, updateNode(root.left, i, value, height, bit - 1), root.right);
         }
     }
-    
-    
-    
 
     public int get(DynamicArray a, int i) {
+        Node node = a.root;
 
-        Node node=a.root;
-        for (int bit =a.height; bit >= 0; bit--) {
+        if (node == null) {
+            throw new IllegalArgumentException("Index " + i + " finns inte i arrayen.");
+        }
+
+        for (int bit = node.height; bit >= 0; bit--) {
             if (((i >>> bit) & 1) == 1){
-                node=node.right;
+                node = node.right;
             } else {
-                node= node.left;
+                node = node.left;
             }
-        }   return node.value;
+        }   
+        return node.value;
     }
 
     public static void main(String[] args) {
 
         DynamicArray tree = new DynamicArray(null);
-        tree.set(tree, 3, 5);
-        tree.get(tree, 3);
+        DynamicArray tree2 = tree.set(tree, 0, 5);
+        tree2.get(tree2, 0);
 
         // Stack treeStack;
 
--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,13 @@
 class Node {
     Integer value;
     Node left, right;
+    int height;
 
-    Node(Integer value, Node left, Node right) {
+    Node(Integer value, Node left, Node right, int height) {
         this.value = value;
         this.left = left;
         this.right = right;
+        this.height = height;
     }
 
     public boolean isLeaf() {
