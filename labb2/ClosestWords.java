1db6275214e536da3d222ad2b6e1777e2522502a
Mattias Nyberg <mattiasnyberg@outlook.com>
2025-09-18 14:59:42 +0200

    klarar 4 test kattis, iterativ dynamisk progmatris, 178 ms

--- labb2/ClosestWords.java
+++ labb2/ClosestWords.java
@@ -1,3 +1,4 @@
+
 /* Labb 2 i DD2350 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbinstruktionerna i kursrummet i Canvas                  */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
@@ -10,20 +11,40 @@ public class ClosestWords {
   int closestDistance = -1;
 
   int partDist(String w1, String w2, int w1len, int w2len) {
-    if (w1len == 0)
-      return w2len;
-    if (w2len == 0)
-      return w1len;
-    int res = partDist(w1, w2, w1len - 1, w2len - 1) + 
-	(w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-    int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
-    if (addLetter < res)
-      res = addLetter;
-    int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
-    if (deleteLetter < res)
-      res = deleteLetter;
-    return res;
+    int[][] M = new int[w1len + 1][w2len + 1];
+
+    for (int i = 0; i < w1len + 1; i++) M[i][0] = i;
+    for (int j = 0; j < w2len + 1; j++) M[0][j] = j;
+
+    for (int i = 1; i < w1len + 1; i++) {
+      for (int j = 1; j < w2len + 1; j++) {
+        if (w1.charAt(i - 1) == w2.charAt(j - 1)) {
+          M[i][j] = M[i - 1][j - 1];
+        } else {
+          int shortest = Math.min(M[i][j - 1], M[i - 1][j - 1]);
+          shortest = Math.min(shortest, M[i - 1][j]);
+          M[i][j] = shortest + 1;
+        }
+      }
+    }
+    return M[w1len][w2len];
   }
+
+  // int partDist(String w1, String w2, int w1len, int w2len) {
+  //   if (w1len == 0)
+  //     return w2len;
+  //   if (w2len == 0)
+  //     return w1len;
+  //   int res = partDist(w1, w2, w1len - 1, w2len - 1) +
+  //       (w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
+  //   int addLetter = partDist(w1, w2, w1len - 1, w2len) + 1;
+  //   if (addLetter < res)
+  //     res = addLetter;
+  //   int deleteLetter = partDist(w1, w2, w1len, w2len - 1) + 1;
+  //   if (deleteLetter < res)
+  //     res = deleteLetter;
+  //   return res;
+  // }
 
   int distance(String w1, String w2) {
     return partDist(w1, w2, w1.length(), w2.length());
@@ -37,8 +58,7 @@ public class ClosestWords {
         closestDistance = dist;
         closestWords = new LinkedList<String>();
         closestWords.add(s);
-      }
-      else if (dist == closestDistance)
+      } else if (dist == closestDistance)
         closestWords.add(s);
     }
   }
