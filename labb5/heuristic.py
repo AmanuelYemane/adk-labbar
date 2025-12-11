ca02d49b767097c65ff56a2b9e205c6453a3e2de
Mattias Nyberg <mattiasnyberg@outlook.com>
2025-12-11 21:47:38 +0100

    kommentarer och tidskomplexitet

--- labb5/heuristic.py
+++ labb5/heuristic.py
@@ -54,34 +54,34 @@ n = read_int() # roller
 s = read_int() # scener
 k = read_int() # vanliga skådisar
 
-actors_in_roles = [[] for _ in range(n + 1)]
-roles_in_scenes = [[] for _ in range(s + 1)]
+actors_in_roles = [[] for _ in range(n + 1)] # O(n)
+roles_in_scenes = [[] for _ in range(s + 1)] # O(s)
 
-used_actors_and_their_roles = [[] for _ in range(k + n)]
-assigned_roles = [0 for _ in range(n + 1)]
+used_actors_and_their_roles = [[] for _ in range(k + n)] # O(k+n)
+assigned_roles = [0 for _ in range(n + 1)] # O(n)
 
-neighbors = [[] for _ in range(n + 1)]
-super_actor_is_used = [False] * (n + 1)
+neighbors = [set() for _ in range(n + 1)] # O(n)
+super_actor_is_used = [False] * (n + 1) # O(n)
 
-for i in range(1, n + 1):
+for i in range(1, n + 1): # O(n)
     actors_in_roles[i] = read_line()
 
-for i in range(1, s + 1):
+for i in range(1, s + 1): # O(s)
     roles_in_scenes[i] = read_line()
 
-# Gå igenom och spara alla roller som möts
-for i in range(1, s + 1):
+# Gå igenom och spara alla roller som möts. O(s*n^2)
+for i in range(1, s + 1): 
     scene_roles = roles_in_scenes[i]
     for r1 in scene_roles:
         for r2 in scene_roles:
             if r1 != r2:
-                neighbors[r1].append(r2)
+                neighbors[r1].add(r2)
 
 diva1_role = 0
 diva2_role = 0
 found_pair = False
 
-# Gå igenom alla roller diva 1 kan ha
+# Gå igenom alla roller diva 1 kan ha. O(n*(k+n*k)) = O(n*k + n^2*k) = O(n^2*k)
 for r1 in range(1, n + 1):
     if 1 in actors_in_roles[r1]:
         
@@ -108,11 +108,11 @@ if found_pair:
     assigned_roles[diva2_role] = 2
     used_actors_and_their_roles[2].append(diva2_role)
 
-# Tilldela resterande roller
-for role in range(1, n + 1):
-    assign_actor_to_role(role)
+# Tilldela resterande roller. O(n*(k*n+n)) = O(n^2*k + n^2) = O(n^2*k)
+for role in range(1, n + 1): # O(n)
+    assign_actor_to_role(role) # *O(k*n+n)
 
-# Räkna ihop antalet använda skådisar
+# Räkna ihop antalet använda skådisar. O(n^2)
 num_of_actors_used = 0
 for roles in used_actors_and_their_roles:
     if len(roles) > 0:
@@ -120,11 +120,15 @@ for roles in used_actors_and_their_roles:
 
 print(num_of_actors_used)
 
-# Skriv ut de använda skådisarna och deras roller
+# Skriv ut de använda skådisarna och deras roller. O(k*n^2)
 for i in range(1, len(used_actors_and_their_roles)):
-    if len(used_actors_and_their_roles[i]) > 0:
+    num_roles = len(used_actors_and_their_roles[i])
+    if num_roles > 0:
         print(
             i,
-            len(used_actors_and_their_roles[i]),
+            num_roles,
             " ".join(str(role) for role in used_actors_and_their_roles[i])
         )
+
+# Tidskomplexitet: O(s*n^2) + O(n^2*k) = O(s*n^2 + n^2*k) = O(n^2*(s+k))
+# Heuristik: Vi väljer vilken skådis som ska spela varje roll genom att välja den första bästa skådisen som inte krockar (girig algoritm). 
